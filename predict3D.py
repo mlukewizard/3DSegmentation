4fa7da9ce735198961b46f1fb0293e74b6a9fc5d
mlukewizard <lukemarkham1383@gmail.com>
2017-11-23 23:52:44 +0000

    Changing OS

--- predict3D.py
+++ predict3D.py
@@ -10,32 +10,39 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
-model_file = '/media/sf_sharedFolder/Models/23rdNov/weights.03-0.03.h5'
+model_file = '/media/sf_sharedFolder/Models/23rdNov/weights.02-0.03.h5'
 
 x = np.load('/media/sf_sharedFolder/npArrays/39894NS/3DAugment001-002PatientNS_Original.npy')
 
 fig = plt.figure()
 #model = load_model(model_file)
-npImageArray = np.ndarray((1, 3, 256, 256, 1), dtype='float32')
+npImageArray = np.ndarray((1, 5, 256, 256, 1), dtype='float32')
 
 for i in range(200, 400, 50):
-    x1 = x[i, 1, :, :, 0]
+    x1 = x[i, 0, :, :, 0]
     x2 = x[i, 1, :, :, 0]
     x3 = x[i, 2, :, :, 0]
+    x4 = x[i, 3, :, :, 0]
+    x5 = x[i, 4, :, :, 0]
     npImageArray[0, :, :, :, 0] = x[i, :, :, :, 0]
 
     #y = model.predict(npImageArray)
-    #y1 = y[0, 1, :, :, 0]
+    #y1 = y[0, 2, :, :, 0]
 
     #plt.subplot(121)
-    #plt.imshow(x1, cmap='gray')
+    #plt.imshow(x3, cmap='gray')
     #plt.subplot(122)
     #plt.imshow(y1, cmap='gray')
     #plt.show()
-    plt.subplot(131)
+
+    plt.subplot(151)
     plt.imshow(x1, cmap='gray')
-    plt.subplot(132)
+    plt.subplot(152)
     plt.imshow(x2, cmap='gray')
-    plt.subplot(133)
+    plt.subplot(153)
     plt.imshow(x3, cmap='gray')
+    plt.subplot(154)
+    plt.imshow(x4, cmap='gray')
+    plt.subplot(155)
+    plt.imshow(x5, cmap='gray')
     plt.show()
