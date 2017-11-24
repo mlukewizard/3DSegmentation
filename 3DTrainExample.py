1fadeec7d7d94934caf3b33480c2daae5eeae31f
mlukewizard <lukemarkham1383@gmail.com>
2017-11-24 15:46:10 +0000

    Added batch normalisation and dropout

--- 3DTrainExample.py
+++ 3DTrainExample.py
@@ -44,52 +44,75 @@ for k in range(10):  # 50 means training for 50 epochs
 
         inputs = Input((5, 256, 256, 1))
 
-        conv1 = TimeDistributed(Conv2D(32, (3, 3), activation='relu', padding='same'))(inputs)
+        conv1 = TimeDistributed(BatchNormalization())(inputs)
+        conv1 = TimeDistributed(Conv2D(32, (3, 3), activation='relu', padding='same'))(conv1)
+        conv1 = TimeDistributed(BatchNormalization())(conv1)
+        conv1 = TimeDistributed(Dropout(0.5))(conv1)
         conv1 = TimeDistributed(Conv2D(32, (3, 3), activation='relu', padding='same'))(conv1)
         pool1 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv1)
 
-        conv2 = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same'))(pool1)
+        conv2 = TimeDistributed(BatchNormalization())(pool1)
+        conv2 = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same'))(conv2)
+        conv2 = TimeDistributed(BatchNormalization())(conv2)
+        conv2 = TimeDistributed(Dropout(0.5))(conv2)
         conv2 = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same'))(conv2)
         pool2 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv2)
 
-        conv3 = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same'))(pool2)
+        conv3 = TimeDistributed(BatchNormalization())(pool2)
+        conv3 = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same'))(conv3)
+        conv3 = TimeDistributed(BatchNormalization())(conv3)
+        conv3 = TimeDistributed(Dropout(0.5))(conv3)
         conv3 = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same'))(conv3)
         pool3 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv3)
 
-        conv4 = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same'))(pool3)
+        conv4 = TimeDistributed(BatchNormalization())(pool3)
+        conv4 = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same'))(conv4)
+        conv4 = TimeDistributed(BatchNormalization())(conv4)
+        conv4 = TimeDistributed(Dropout(0.5))(conv4)
         conv4 = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same'))(conv4)
         pool4 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv4)
 
         myLSTM = Bidirectional(ConvLSTM2D(512, (3, 3), activation='relu', padding='same', return_sequences=True))(pool4)
 
         up6 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(myLSTM), conv4], axis=4)
-        conv6 = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same'))(up6)
+        conv6 = TimeDistributed(BatchNormalization())(up6)
+        conv6 = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same'))(conv6)
+        conv6 = TimeDistributed(BatchNormalization())(conv6)
+        conv6 = TimeDistributed(Dropout(0.5))(conv6)
         conv6 = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same'))(conv6)
 
         up7 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(conv6), conv3], axis=4)
-        conv7 = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same'))(up7)
+        conv7 = TimeDistributed(BatchNormalization())(up7)
+        conv7 = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same'))(conv7)
+        conv7 = TimeDistributed(BatchNormalization())(conv7)
+        conv7 = TimeDistributed(Dropout(0.5))(conv7)
         conv7 = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same'))(conv7)
 
         up8 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(conv7), conv2], axis=4)
-        conv8 = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same'))(up8)
+        conv8 = TimeDistributed(BatchNormalization())(up8)
+        conv8 = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same'))(conv8)
+        conv8 = TimeDistributed(BatchNormalization())(conv8)
+        conv8 = TimeDistributed(Dropout(0.5))(conv8)
         conv8 = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same'))(conv8)
 
         up9 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(conv8), conv1], axis=4)
-        conv9 = TimeDistributed(Conv2D(32, (3, 3), activation='relu', padding='same'))(up9)
+        conv9 = TimeDistributed(BatchNormalization())(up9)
+        conv9 = TimeDistributed(Conv2D(32, (3, 3), activation='relu', padding='same'))(conv9)
+        conv9 = TimeDistributed(BatchNormalization())(conv9)
+        conv9 = TimeDistributed(Dropout(0.5))(conv9)
         conv9 = TimeDistributed(Conv2D(32, (3, 3), activation='relu', padding='same'))(conv9)
 
-        conv10 = TimeDistributed(Conv2D(1, (1, 1), activation='sigmoid'))(conv9)
+        conv10 = TimeDistributed(BatchNormalization())(conv9)
+        conv10 = TimeDistributed(Dropout(0.5))(conv10)
+        conv10 = TimeDistributed(Conv2D(1, (1, 1), activation='sigmoid'))(conv10)
 
         model = Model(inputs=[inputs], outputs=[conv10])
-        #model.summary()
-        #model.compile(optimizer=Adam(lr=1e-4), loss = losses.binary_crossentropy)
- 	model.compile(optimizer=Adam(lr=1e-4), loss = my_loss)  
 	
 	epoch_number = 0
 
     else:
 
-        currentMax = 0;
+        currentMax = 0
         for fn in model_list:
             epoch_number = int(fn.split('weights.')[1].split('-')[0])
             if epoch_number > currentMax:
@@ -102,7 +125,8 @@ for k in range(10):  # 50 means training for 50 epochs
         f_model.close()
         model = load_model(os.path.join(model_folder, model_file))
         print('Using model number ' + str(epoch_number))
-        model.compile(optimizer=Adam(lr=5e-5), loss=losses.binary_crossentropy)
+
+    model.compile(optimizer=Adam(lr=1e-4), loss=my_loss)
 
     model_check_file = os.path.join(model_folder, 'weights.{epoch:02d}-{loss:.2f}.h5')
 
